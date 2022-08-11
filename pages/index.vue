9262df9e211ca530b7edf50d68de78dc9b466b54
andreamarin <andrea.marin2411@gmail.com>
2022-08-11 01:39:35 -0500

    fixed menu button

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="holder">
-    <Slide :close-on-navigation="true" no-overlay>
-      <a id="landing-menu" href="#landing">
-        <span>Inicio</span>
-      </a>
-      <a id="location-menu" href="#location">
-        <span>Lugar</span>
-      </a>
-      <a id="dressCode-menu" href="#dress-code">
-        <span>Vestimenta</span>
-      </a>
-      <a id="faq-menu" href="#faq">
-        <span>Preguntas frecuentes</span>
-      </a>
-    </Slide>
     <div class="all-sections">
       <div id="landing" class="main-section">
         <landing />
       </div>
-      <!-- <div id="location" class="section">
+      <div id="location" class="section">
         <location />
-      </div> -->
+      </div>
+    </div>
+    <div class="landing-menu-container">
+      <Slide :close-on-navigation="true" no-overlay>
+        <a id="landing-menu" href="#landing">
+          <span>Inicio</span>
+        </a>
+        <a id="location-menu" href="#location">
+          <span>Lugar</span>
+        </a>
+        <a id="dressCode-menu" href="#dress-code">
+          <span>Vestimenta</span>
+        </a>
+        <a id="faq-menu" href="#faq">
+          <span>Preguntas frecuentes</span>
+        </a>
+      </Slide>
     </div>
   </div>
 </template>
@@ -46,7 +48,14 @@ export default {
 
 <style>
 .holder{
+  display: block;
   position: relative;
+  height: 100%;
+}
+
+.landing-menu-container{
+  position: fixed;
+  top: 5px;
 }
 
 .all-sections {
@@ -62,10 +71,15 @@ export default {
 }
 
 .section {
-  height: 500px;
+  min-height: 100vh;
   width: 100%;
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
+/* burger menu config */
 .bm-burger-bars {
   background-color: var(--dark-accent);
 }
@@ -80,6 +94,27 @@ export default {
 }
 
 .bm-burger-button{
-  position: relative;
+  background-color: var(--light-accent);
+  border-radius: 25px;
+  width: 85px;
+  height: 50px;
+}
+
+.line-style {
+  height: 12%;
+  width: 60%;
+  margin: auto;
+}
+
+div.bm-burger-button > :first-child {
+  top: 25% !important;
+}
+
+div.bm-burger-button > :nth-child(2) {
+  top: 45% !important;
+}
+
+div.bm-burger-button > :last-child {
+  top: 65% !important;
 }
 </style>
