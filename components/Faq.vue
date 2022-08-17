ddc20e7c071b381504e8a990b56d8d9d079f137e
andreamarin <andrea.marin2411@gmail.com>
2022-08-16 23:13:04 -0500

    change questions order

--- components/Faq.vue
+++ components/Faq.vue
@@ -31,13 +31,13 @@ export default {
   data () {
     return {
       questions: [
+        { q: 'Hay fecha límite para confirmar mi asistencia?', a: 'Por favor avísanos a más tardar el 8 de septiembre para que podamos tener un conteo preciso.' },
         { q: 'Habrá comida?', a: 'Sí, a las 14:00 se servirá la comida.' },
         { q: 'Cuentan con menú infantil y/o vegetariano?', a: 'Sí hay alternativas infantil y vegetariana pero solo bajo pedido. Te pedimos nos avises lo antes posible si lo requieres.' },
         { q: 'Qué habrá de tomar?', a: 'Refrescos, agua mineral, agua tónica y además, el salón ofrece barra libre nacional (tequila, ron, whisky, vodka y brandy).' },
         { q: 'Habrá descorche libre?', a: 'Sí, puedes llevar el alcohol adicional que gustes, siempre y cuando sean botellas marbetadas.' },
         { q: 'A qué hora termina el salón?', a: 'La fiesta en el salón acaba a las 19:00 hrs' },
         { q: 'Hay estacionamiento?', a: 'Sí, el salón cuenta con el servicio adicional de valet parking.' },
-        { q: 'Hay fecha límite para confirmar mi asistencia?', a: 'Por favor avísanos a más tardar el 8 de septiembre para que podamos tener un conteo preciso.' },
         { q: 'Puedo apoyar en algo?', a: 'Participando al máximo en el evento y una gran actitud para hacer de este, el mejor cumpleaños' }
       ]
 
